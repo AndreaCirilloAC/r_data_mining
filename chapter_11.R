27b9063a46311aae8f935dbc404371f2c1dc5986
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-11-03 05:45:59 +0100

    introduced rf passing categorical as factors

--- chapter_11.R
+++ chapter_11.R
@@ -1,2 +1,14 @@
 library(dplyr)
 library(tidyr)
+
+
+#random forest
+rf.form <- as.formula(paste("as.factor(default_numeric)", 
+                            paste(colnames(training_data)[-c(4,11,12)], 
+                                  sep = " + ",collapse = " + "), sep = " ~ "))
+training_data %>% 
+  mutate(commercial_portfolio = as.factor(commercial_portfolio),
+         business_unit = as.factor(business_unit))-> training_data_factor
+set.seed(11)
+random_forest <- randomForest::randomForest(formula = as.factor(default_numeric)~., 
+                                            data = training_data_factor, ntree = 200, importance = TRUE) # ok ma attenzione che qui ci mancano le variabili categoriche
