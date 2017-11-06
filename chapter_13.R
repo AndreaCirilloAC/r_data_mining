bb3dfeeaa7de994ead27284039cf1af137524883
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-11-06 06:37:54 +0100

    added chapter 13

---
+++ chapter_13.R
@@ -0,0 +1,11 @@
+# text data mining
+
+# import companies credit bureau information, stored in PDF format
+
+# dataframe from PDF
+
+# descriptive: most common industries
+
+# network analysis on  shareholders
+
+# topic analysis on notes 
