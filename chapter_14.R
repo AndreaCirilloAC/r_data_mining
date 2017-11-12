95277f871c7fc0ce28163ca63ba037a5181151fe
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-11-13 00:08:23 +0100

    simplified server side

--- chapter_14.R
+++ chapter_14.R
@@ -2,13 +2,13 @@
 
 ui <- fluidPage(
   numericInput("num", label = h3("Numeric input"), value = 100),
-  renderPlot("plot")
+  plotOutput("plot")
 )
 
 server <- function(input, output, session) {
-size <- reactive({input$num})
-  output$plot <- plotOutput({ 
-    rnorm(size()) %>% hist() })
+
+  output$plot <- renderPlot({ 
+    rnorm(input$num) %>% hist() })
 }
 
 shinyApp(ui, server)
